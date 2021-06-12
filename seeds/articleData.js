0a5db31868d93baa7512efe440c5c95d14b2b6df
Statikman619 <martinez.michael02@gmail.com>
2021-06-12 11:57:26 -0700

    started my blog

---
+++ seeds/articleData.js
@@ -0,0 +1,24 @@
+const { Article } = require('../models');
+
+const articledata = [
+  {
+    title: 'Comics and Anime',
+    content: 'This is the launch of my new blog!',
+    author_id: 1,
+  },
+  {
+    title: 'Is Spider-Man the best superhero ever?',
+    content: 'Some people prefer Superman but they might be wrong'
+    author_id: 2,
+  },
+  {
+    title: 'Is Goku the strongest Anime character?',
+    content: 'He may not be the strongest but he has the most potential?',
+    author_id: 3,
+  },
+  
+];
+
+const seedArticle = () => Article.bulkCreate(articledata);
+
+module.exports = seedArticle;
